16bab7d3964364e4220eae62585b9ec78397e4f4
hushufeng <shufeng.hu@seekfangle.com>
2017-12-28 18:35:46 +0800

    threeTotateString

---
+++ threeTotateString.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "string.h"
+
+void ReverseString(char* s, int from, int to)
+{
+  while (from < to)
+  {
+    char t = s[from];
+    s[from++] = s[to];
+    s[to--] = t;
+  }
+}
+
+void LeftRotateString(char* s, int n, int m)
+{
+  m %=n;
+  ReverseString(s, 0, m - 1);
+  ReverseString(s, m, n - 1);
+  ReverseString(s, 0, n - 1);
+}
+
+int main() {
+  char str[7] = "abcdefg";
+  LeftRotateString(str,strlen(str),3);
+  printf("%s\n", str);
+  return 0;
+}
